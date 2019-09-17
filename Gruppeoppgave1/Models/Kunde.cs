198a8eef9a5857c16e20710d453daab3838c4a7f
Cato Akay <catohilmi@hotmail.com>
2019-09-17 12:43:36 +0200

    Kunde klasse fysisk kopiert

---
+++ Gruppeoppgave1/Models/Kunde.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gruppeoppgave1.Models
+{
+    public class Kunde
+    {
+        public int ID { get; set; }
+        public String Fornavn { get; set; }
+        public String Etternavn { get; set; }
+        public int Fodselsdato { get; set; }
+    }
+}
