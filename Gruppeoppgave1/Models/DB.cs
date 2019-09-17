6fdbbf116f3812f91f4f763a14495806c006676f
Bao <bao91@protonmail.com>
2019-09-17 12:06:55 +0200

    Opprettet Kundeklasse

--- Gruppeoppgave1/Models/DB.cs
+++ Gruppeoppgave1/Models/DB.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace Gruppeoppgave1.Models
 {
-    public class DB:DBContext
+    public class DB : DbContext
     {
         public DB() : base("name=Vy")
         {
             Database.CreateIfNotExists();
+        }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     }
 }
