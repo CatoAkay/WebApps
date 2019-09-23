15ad0a2fb06bfaef2a612b4b880434123467eb87
Bao <bao91@protonmail.com>
2019-09-23 22:03:41 +0200

    added suggestion on search field
    bao

---
+++ Gruppeoppgave1/Scripts/site/main.js
@@ -0,0 +1,60 @@
+﻿$(function () {
+
+    const debounce = (func, delay) => {
+        let inDebounce
+        return function () {
+            const context = this
+            const args = arguments
+            clearTimeout(inDebounce)
+            inDebounce = setTimeout(() => func.apply(context, args), delay)
+        }
+    }
+
+    let stopQuery = function (input) {
+        var data = JSON.stringify({
+            query: `query ($stopInput: String!)  {
+                    stopPlace(size: 5, stopPlaceType: railStation, query: $stopInput) {
+                        id
+                        name {
+                            value
+                        }
+                    }
+                }`,
+            variables: { "stopInput": input.target.value }
+
+        });
+        $.ajax({
+            method: "POST",
+            contentType: "application/json",
+            url: "https://api.entur.io/stop-places/v1/graphql",
+            data: data,
+            success: function (response) {
+                var id = "#" + input.target.attributes.list.value;
+                $(id).html("");
+                response.data.stopPlace.forEach(function (item) {
+                    $(id).append('<option data-value="' + item.id + '">' + item.name.value + '</option>');
+                });
+                console.log(response.data);
+            },
+            error: function (response) {
+                console.log(response);
+            }
+        });
+    }
+
+    $('.stopInput').on('input', debounce(stopQuery, 500));
+
+   
+
+
+
+    /*
+    var paramter = { url: "/Api" };
+    var funksjonen = function (x) {
+        alert(data)
+    });
+  
+    $.ajax(paramter).done(funksjonen);
+    */
+
+});
