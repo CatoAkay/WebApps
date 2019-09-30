c4d3a1958732b0ffaf2e2b97f00660dd2e8e6402
Bao <bao91@protonmail.com>
2019-09-30 12:51:35 +0200

    Lagt til billettmodell

---
+++ Gruppeoppgave1/Models/Billett.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gruppeoppgave1.Models
+{
+	public class Billett
+	{
+		public int ID { get; set; }
+		public String Fra { get; set; }
+		public String Til { get; set; }
+		public String Dato { get; set; }
+		public String Tid { get; set; }
+		public Double Pris { get; set; }
+		public virtual Kunde kunde { get; set; }
+	}
+}
