24704c874b7451e46d76dc21a49cb2c0afa69794
Bjørnar Hoff <bjornarhoff@gmail.com>
2019-10-21 14:57:20 +0200

    Logging til fil

--- DAL/DatabaseLogikkDAL.cs
+++ DAL/DatabaseLogikkDAL.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using Model;
@@ -18,7 +19,7 @@ namespace DAL
         {
             Billett billett = new Billett
             {
-                Reise = info.reise, 
+                Reise = info.reise,
                 Kunde = info.kunde
             };
             db.Billett.Add(billett);
@@ -31,19 +32,19 @@ namespace DAL
         {
             return db.Billett.Find(id);
         }
-        
+
         public IEnumerable<Kunde> getAlleKunder()
         {
             IEnumerable<Kunde> allebilleter = db.Kunde;
             return allebilleter;
-        }         
-        
+        }
+
         public IEnumerable<Admin> getAlleAdmin()
         {
             IEnumerable<Admin> alleAdmin = db.Admin;
             return alleAdmin;
-        } 
-        
+        }
+
         public void lagAdmin(Admin admin)
         {
             DB db = new DB();
@@ -112,23 +113,34 @@ namespace DAL
             db.Kredittkort.Remove(kredidkort);
             db.SaveChanges();
         }
-        
-        public bool Autorisasjon(Admin admin)
-        { 
-            using (db)
-            {
-                var adminDetail = db.Admin.FirstOrDefault(x => x.Brukernavn == admin.Brukernavn && x.Passord == admin.Passord);
-                if (adminDetail == null)
-                { 
-                    admin.loginMsgError = "Ikke gyldig brukernavn eller passord";
-                    return true;
 
-                }
-                else
+        public bool Autorisasjon(Admin admin)
+        {
+            try
+            {
+                using (db)
                 {
-                    return false;
+                    var adminDetail = db.Admin.FirstOrDefault(x =>
+                        x.Brukernavn == admin.Brukernavn && x.Passord == admin.Passord);
+                    if (adminDetail == null)
+                    {
+                        admin.loginMsgError = "Ikke gyldig brukernavn eller passord";
+                        return true;
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
+
             }
+            catch (Exception ex)
+            {
+                Logging.errorTilFil(ex);
+            }
+
+            return true;
         }
     }
 }
--- Model/Logging.cs
+++ Model/Logging.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Model
 {
@@ -11,5 +12,59 @@ namespace Model
         public string GammelVerdi { get; set; }
         public string NyVerdi { get; set; }
         public DateTime DatoEndret { get; set; }
+
+
+
+
+        private static String ErrorLinje, ErrorMsg, ErrorType, ErrorUrl, HostIp, ErrorLokasjon, HostAdd;
+
+        public static void errorTilFil(Exception e)
+        {
+            var linje = Environment.NewLine + Environment.NewLine;
+
+            ErrorLinje = e.StackTrace.Substring(e.StackTrace.Length - 7, 7);
+            ErrorMsg = e.GetType().Name.ToString();
+            ErrorType = e.GetType().ToString();
+            ErrorLokasjon = e.Message.ToString();
+
+            try
+            {
+                string filepath = @"C:\Error.txt";
+                if (!Directory.Exists(filepath))
+                {
+                    Directory.CreateDirectory(filepath);
+                }
+
+                filepath = filepath + DateTime.Today.ToString("dd-MM-yy") + ".txt";
+
+                if (!File.Exists(filepath))
+                {
+                    File.Create(filepath).Dispose();
+                }
+
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    var error = "Logg dato: " + " " + DateTime.Now.ToString() + linje + "Error linje nr: " + " " +
+                                   ErrorLinje + linje + "Error melding: " + ErrorMsg + linje + "Error type: " + " " +
+                                   ErrorType + linje + "Error lokasjon: " + " " + ErrorLokasjon + linje +
+                                   "Bruker ip: " + " " + HostIp + linje;
+                    sw.WriteLine("Exception ved datoen:  " + " " + DateTime.Now.ToString());
+                    sw.WriteLine(linje);
+                    sw.WriteLine(error);
+                    sw.WriteLine();
+
+                    sw.Flush();
+                    sw.Close();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
     }
-}
+
+ }
